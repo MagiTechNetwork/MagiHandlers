9d625ad0af758be82f79fd52a60b4238f76cbd01
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-05-13 13:11:51 -0300

    Added witchery support and blocked 2 rituals.

---
+++ src/main/java/net/heyzeer0/mgh/mixins/witchery/MixinStepWeatherCallStorm.java
@@ -0,0 +1,23 @@
+package net.heyzeer0.mgh.mixins.witchery;
+
+import com.emoniph.witchery.blocks.BlockCircle;
+import com.emoniph.witchery.ritual.RitualStep;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+
+/**
+ * Created by HeyZeer0 on 13/05/2017.
+ * Copyright © HeyZeer0 - 2016
+ */
+@Pseudo
+@Mixin(targets = "com/emoniph/witchery/ritual/rites/RiteWeatherCallStorm$StepWeatherCallStorm", remap = false)
+public abstract class MixinStepWeatherCallStorm {
+
+    @Overwrite
+    public RitualStep.Result process(World world, int posX, int posY, int posZ, long ticks, BlockCircle.TileEntityCircle.ActivatedRitual ritual) {
+        return RitualStep.Result.ABORTED_REFUND;
+    }
+
+}
