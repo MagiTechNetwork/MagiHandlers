735e946c7345bcaf80b65f8ff6a16054221c235f
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-05-20 16:17:12 -0300

    Added extrautilities support and closes #8

---
+++ src/main/java/net/heyzeer0/mgh/mixins/extrautilities/MixinItemBlockDrum.java
@@ -0,0 +1,39 @@
+package net.heyzeer0.mgh.mixins.extrautilities;
+
+import com.rwtema.extrautils.item.ItemBlockMetadata;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.IFluidContainerItem;
+import net.minecraftforge.fluids.ItemFluidContainer;
+import openblocks.common.tileentity.TileEntityTank;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * Created by HeyZeer0 on 20/05/2017.
+ * Copyright © HeyZeer0 - 2016
+ */
+@Pseudo
+@Mixin(targets = "com/rwtema/extrautils/item/ItemBlockDrum", remap = false)
+public abstract class MixinItemBlockDrum extends ItemBlockMetadata implements IFluidContainerItem {
+
+    public MixinItemBlockDrum(Block b) {
+        super(b);
+    }
+
+    @Shadow
+    public ItemFluidContainer slaveItem;
+
+    @Overwrite
+    public int fill(ItemStack container, FluidStack resource, boolean doFill) {
+        int to_add = resource.amount / container.stackSize;
+
+        this.slaveItem.fill(container, new FluidStack(resource.getFluid(), (to_add > 0 ? to_add : 0)), doFill);
+        return resource.amount;
+    }
+
+}
