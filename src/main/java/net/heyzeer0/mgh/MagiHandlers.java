d5e7b4316f7c80f832378c08009af7c8aaa5f3bc
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-05-22 17:41:35 -0300

    Cleared inused improts.

--- src/main/java/net/heyzeer0/mgh/MagiHandlers.java
+++ src/main/java/net/heyzeer0/mgh/MagiHandlers.java
@@ -6,7 +6,6 @@ import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.EventPriority;
 
 /**
  * Created by HeyZeer0 on 08/03/2017.
