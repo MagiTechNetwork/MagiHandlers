b5896f167933693ed3805b61b98bc6ab92328dae
Eufranio <franinho.anjo@live.com>
2017-06-09 18:57:48 -0300

    teste

---
+++ src/main/java/net/heyzeer0/mgh/mixins/avaritia/MixinBreakAvaritiaBlock.java
@@ -0,0 +1,18 @@
+package net.heyzeer0.mgh.mixins.avaritia;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "fox/spiteful/avaritia/entity/EntityGapingVoid", remap = false)
+
+public abstract class MixinBreakAvaritiaBlock {
+
+    @Inject(method = "onUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockToAir(III)Z"))
+    private void injectQuebrando(CallbackInfo ci){
+        System.out.println("QUEBRANDOOOOO");
+    }
+}
