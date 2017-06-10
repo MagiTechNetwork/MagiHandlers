b7a034a41183fd18969cb9f92b69e568f6b6a090
Eufranio <franinho.anjo@live.com>
2017-06-10 10:51:39 -0300

    Added support for Avaritia and fixed Endest Pearl

---
+++ src/main/java/net/heyzeer0/mgh/mixins/avaritia/MixinEndestPearl.java
@@ -0,0 +1,21 @@
+package net.heyzeer0.mgh.mixins.avaritia;
+
+import net.minecraft.util.MovingObjectPosition;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created by Frani on 10/06/2017.
+ */
+
+@Pseudo
+@Mixin(targets = "fox/spiteful/avaritia/entity/EntityEndestPearl", remap = false)
+public abstract class MixinEndestPearl {
+    @Inject(method = "func_70184_a", at = @At("HEAD"), cancellable = true)
+    private void injectImpact(MovingObjectPosition mop, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
