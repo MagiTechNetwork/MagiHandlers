d17f7ff8a93be9f115247d8cdcb2da640e8c7491
Eufranio <franinho.anjo@live.com>
2017-06-29 00:30:50 -0300

    Increased health of Cultist Leader to 500, and changed it's base damage to 15. Progress to #52

---
+++ src/main/java/net/heyzeer0/mgh/mixins/thaumcraft/MixinEntityCultistLeader.java
@@ -0,0 +1,30 @@
+package net.heyzeer0.mgh.mixins.thaumcraft;
+
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+
+/**
+ * Created by Frani on 29/06/2017.
+ */
+
+@Pseudo
+@Mixin(targets = "thaumcraft/common/entities/monster/boss/EntityCultistLeader", remap = false)
+public abstract class MixinEntityCultistLeader extends EntityMob {
+
+    public MixinEntityCultistLeader(World world) {
+        super(world);
+    }
+
+    @Overwrite
+    protected void func_110147_ax() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.32D);
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(500.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(15.0D);
+    }
+
+}
