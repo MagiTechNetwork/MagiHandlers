b476b970237f3fe7b330358ac5a955259cf66582
Eufranio <franinho.anjo@live.com>
2017-06-28 21:54:40 -0300

    Added inventory name for Ex Compressum TEs, closes #68

---
+++ src/main/java/net/heyzeer0/mgh/mixins/excompressum/MixinTileEntityAutoHammer.java
@@ -0,0 +1,20 @@
+package net.heyzeer0.mgh.mixins.excompressum;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+
+/**
+ * Created by Frani on 28/06/2017.
+ */
+
+@Pseudo
+@Mixin(targets = "net/blay09/mods/excompressum/tile/TileEntityAutoHammer", remap = false)
+public abstract class MixinTileEntityAutoHammer {
+
+    @Overwrite
+    public String func_145825_b() {
+        return "Auto Hammer";
+    }
+
+}
