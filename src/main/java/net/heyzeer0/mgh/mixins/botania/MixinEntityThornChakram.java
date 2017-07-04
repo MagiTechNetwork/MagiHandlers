764a7dd8a9c0339ea4480dd6c981fbaa7240f8c8
Eufranio <franinho.anjo@live.com>
2017-07-03 21:46:41 -0300

    Fixes Thorn Chakram, closes #21

---
+++ src/main/java/net/heyzeer0/mgh/mixins/botania/MixinEntityThornChakram.java
@@ -0,0 +1,40 @@
+package net.heyzeer0.mgh.mixins.botania;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created by Frani on 03/07/2017.
+ */
+
+@Pseudo
+@Mixin(targets = "vazkii/botania/common/entity/EntityThornChakram", remap = false)
+public abstract class MixinEntityThornChakram extends EntityThrowable {
+
+    public MixinEntityThornChakram(World world) {
+        super(world);
+    }
+
+    @Inject(method = "func_70184_a", at = @At("HEAD"), cancellable = true)
+    private void injectImpact(MovingObjectPosition pos, CallbackInfo ci) {
+        if (pos.entityHit != null && pos.entityHit instanceof EntityLivingBase && pos.entityHit != getThrower() && getThrower() instanceof EntityPlayer) {
+            AttackEntityEvent evt = new AttackEntityEvent((EntityPlayer) getThrower(), pos.entityHit);
+            MinecraftForge.EVENT_BUS.post(evt);
+            if (evt.isCanceled()) {
+                ci.cancel();
+            }
+        }
+    }
+
+}
+
