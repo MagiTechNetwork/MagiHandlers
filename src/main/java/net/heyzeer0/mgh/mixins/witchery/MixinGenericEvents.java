02e08f77e2ef814d5f569a071c2356f6371c0b3d
Eufranio <franinho.anjo@live.com>
2017-08-05 20:22:59 -0300

    Use Bukkit's chat event for Witchery Reflections (closes #87)

--- src/main/java/net/heyzeer0/mgh/mixins/witchery/MixinGenericEvents.java
+++ src/main/java/net/heyzeer0/mgh/mixins/witchery/MixinGenericEvents.java
@@ -1,16 +1,25 @@
 package net.heyzeer0.mgh.mixins.witchery;
 
 import com.emoniph.witchery.common.ExtendedPlayer;
+import com.emoniph.witchery.util.Config;
+import com.emoniph.witchery.util.TransformCreature;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.heyzeer0.mgh.mixins.MixinManager;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by HeyZeer0 on 13/05/2017.
@@ -29,4 +38,20 @@ public abstract class MixinGenericEvents {
         }
     }
 
+    @SubscribeEvent
+    @Overwrite
+    public void onServerChat(ServerChatEvent event) {
+        boolean chatMasqueradeAllowed = Config.instance().allowChatMasquerading;
+        ExtendedPlayer playerEx = ExtendedPlayer.get(event.player);
+        if (playerEx != null && chatMasqueradeAllowed && playerEx.getCreatureType() == TransformCreature.PLAYER && playerEx.getOtherPlayerSkin() != null && !playerEx.getOtherPlayerSkin().isEmpty()) {
+            event.setCanceled(true);
+
+            Set<Player> players = new HashSet<Player>();
+            Player sender = Bukkit.getOfflinePlayer(playerEx.getOtherPlayerSkin()).getPlayer();
+            players.addAll(Bukkit.getServer().getOnlinePlayers());
+
+            AsyncPlayerChatEvent e = new AsyncPlayerChatEvent(false, sender, event.message, players);
+            Bukkit.getPluginManager().callEvent(e);
+        }
+    }
 }
