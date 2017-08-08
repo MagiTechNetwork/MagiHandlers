9c2d17f7bbf4c78686cfb2c74b0866e6c13675fa
Eufranio <franinho.anjo@live.com>
2017-08-08 18:14:24 -0300

    Added setBlockToAir to BlockHelper and fixed #53, OMG

--- src/main/java/net/heyzeer0/mgh/hacks/BlockHelper.java
+++ src/main/java/net/heyzeer0/mgh/hacks/BlockHelper.java
@@ -2,9 +2,12 @@ package net.heyzeer0.mgh.hacks;
 
 import net.heyzeer0.mgh.mixins.MixinManager;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.FakePlayerFactory;
 import net.minecraftforge.event.world.BlockEvent;
 
 /**
@@ -16,8 +19,17 @@ public abstract class BlockHelper {
         BlockEvent.BreakEvent evt = MixinManager.generateBlockEvent(x, y, z, world, owner);
         MinecraftForge.EVENT_BUS.post(evt);
         if(!evt.isCanceled()) {
-            world.setBlock(x, y, z, block);
-            return true;
+            return world.setBlock(x, y, z, block);
+        }
+        return false;
+    }
+
+    public static boolean setBlockToAirWithOwner(int x, int y, int z, World world, Entity entity) {
+        EntityPlayer player = (entity instanceof EntityPlayer ? (EntityPlayer) entity : FakePlayerFactory.getMinecraft((WorldServer) world));
+        BlockEvent.BreakEvent e = MixinManager.generateBlockEvent(x, y, z, world, player);
+        MinecraftForge.EVENT_BUS.post(e);
+        if(!e.isCanceled()) {
+            return world.setBlockToAir(x, y, z);
         }
         return false;
     }
