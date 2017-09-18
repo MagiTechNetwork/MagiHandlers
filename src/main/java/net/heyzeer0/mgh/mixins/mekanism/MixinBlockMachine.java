87d20b1eaedf749fd03792dc65300ff39e0b20d9
Eufranio <frani@magitechserver.com>
2017-09-18 19:44:01 -0300

    Fix Digital Miner crash

--- src/main/java/net/heyzeer0/mgh/mixins/mekanism/MixinBlockMachine.java
+++ src/main/java/net/heyzeer0/mgh/mixins/mekanism/MixinBlockMachine.java
@@ -1,16 +1,16 @@
 package net.heyzeer0.mgh.mixins.mekanism;
 
-import mekanism.common.block.BlockMachine;
 import net.heyzeer0.mgh.hacks.ITileEntityOwnable;
-import net.minecraft.block.BlockContainer;
-import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Created by Frani on 13/08/2017.
@@ -18,29 +18,17 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Pseudo
 @Mixin(targets = "mekanism/common/block/BlockMachine", remap = false)
-public abstract class MixinBlockMachine extends BlockContainer {
+public abstract class MixinBlockMachine {
 
-    public MixinBlockMachine(Material m) {
-        super(m);
-    }
-
-    EntityPlayer owner;
-
-    @Shadow public BlockMachine.MachineBlock blockType;
-
-    @Overwrite
-    @Override
-    public TileEntity createTileEntity(World world, int metadata) {
-        if (BlockMachine.MachineType.get(blockType, metadata) == null) {
-            return null;
+    @Inject(method = "func_149689_a", at = @At("HEAD"))
+    public void onBlockPlacedByOwner(World world, int x, int y, int z, EntityLivingBase entityliving, ItemStack itemstack, CallbackInfo ci) {
+        if (entityliving instanceof EntityPlayer) {
+            EntityPlayer $owner = (EntityPlayer) entityliving;
+            TileEntity $te = world.getTileEntity(x, y, z);
+            if ($te instanceof ITileEntityOwnable) {
+                ((ITileEntityOwnable) $te).setOwner($owner.getCommandSenderName());
+                ((ITileEntityOwnable) $te).setUUID($owner.getUniqueID().toString());
+            }
         }
-
-        TileEntity te = BlockMachine.MachineType.get(blockType, metadata).create();
-        if (te instanceof ITileEntityOwnable && this.owner != null) {
-            ((ITileEntityOwnable) te).setOwner(owner.getCommandSenderName());
-            ((ITileEntityOwnable) te).setUUID(owner.getUniqueID().toString());
-        }
-
-        return te;
     }
 }
