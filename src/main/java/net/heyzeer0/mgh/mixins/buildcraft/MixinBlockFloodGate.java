3c190512289fadca1c727996a41cdce4311ab0bf
Eufranio <frani@magitechserver.com>
2017-09-18 19:35:43 -0300

    Fix Flood Gate crash

--- src/main/java/net/heyzeer0/mgh/mixins/buildcraft/MixinBlockFloodGate.java
+++ src/main/java/net/heyzeer0/mgh/mixins/buildcraft/MixinBlockFloodGate.java
@@ -1,6 +1,5 @@
 package net.heyzeer0.mgh.mixins.buildcraft;
 
-import buildcraft.factory.TileFloodGate;
 import net.heyzeer0.mgh.hacks.ITileEntityOwnable;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -10,7 +9,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
 
 /**
@@ -21,8 +19,6 @@ import org.spongepowered.asm.mixin.Pseudo;
 @Mixin(targets = "buildcraft/factory/BlockFloodGate", remap = false)
 public class MixinBlockFloodGate extends Block {
 
-    EntityPlayer owner;
-
     public MixinBlockFloodGate(Material m) {
         super(m);
     }
@@ -30,18 +26,13 @@ public class MixinBlockFloodGate extends Block {
     @Override
     public void onBlockPlacedBy(World w, int par2, int par3, int par4, EntityLivingBase e, ItemStack stack) {
         if(e instanceof EntityPlayer) {
-            this.owner = (EntityPlayer) e;
+            EntityPlayer owner = (EntityPlayer) e;
+            TileEntity te = w.getTileEntity(par2, par3, par4);
+            if (te instanceof ITileEntityOwnable) {
+                ((ITileEntityOwnable) te).setOwner(owner.getCommandSenderName());
+                ((ITileEntityOwnable) te).setUUID(owner.getUniqueID().toString());
+            }
         }
-    }
-
-    @Overwrite
-    public TileEntity func_149915_a(final World world, final int metadata) {
-        TileEntity te = new TileFloodGate();
-        if(te instanceof ITileEntityOwnable) {
-            ((ITileEntityOwnable) te).setOwner(owner.getCommandSenderName());
-            ((ITileEntityOwnable) te).setUUID(owner.getUniqueID().toString());
-        }
-        return te;
     }
 
 }
