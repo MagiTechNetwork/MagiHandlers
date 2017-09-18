85887a7528b359134caa86fcbafa6c688d2fa79e
Eufranio <franinho.anjo@live.com>
2017-09-18 19:06:05 -0300

    Fix OreExcavation crash

---
+++ src/main/java/net/heyzeer0/mgh/mixins/oreexcavation/MixinBlockHistory.java
@@ -0,0 +1,25 @@
+package net.heyzeer0.mgh.mixins.oreexcavation;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Created by Frani on 18/09/2017.
+ */
+
+@Pseudo
+@Mixin(targets = "oreexcavation/undo/BlockHistory", remap = false)
+public abstract class MixinBlockHistory {
+
+    @Redirect(method = "Loreexcavation/undo/BlockHistory;<init>(Lnet/minecraftforge/common/util/BlockSnapshot;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/tileentity/TileEntity;writeToNBT(Lnet/minecraft/nbt/NBTTagCompound;)V"))
+    public void onWriteToNBT(TileEntity tile, NBTTagCompound nbt) {
+        if (!tile.getClass().getName().equals("codechicken.multipart.handler.MultipartSaveLoad$TileNBTContainer")) {
+            tile.writeToNBT(nbt);
+        }
+    }
+
+}
