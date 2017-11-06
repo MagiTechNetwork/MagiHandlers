d23ac7eb036b71ef9ebe1df9cc7e07f955475cf4
Eufranio <frani@magitechserver.com>
2017-11-06 18:43:57 -0200

    Check for null entities on explosions, fixes Runic Matrixes disappearing

--- src/main/java/net/heyzeer0/mgh/mixins/forge/MixinExplosion.java
+++ src/main/java/net/heyzeer0/mgh/mixins/forge/MixinExplosion.java
@@ -1,8 +1,11 @@
 package net.heyzeer0.mgh.mixins.forge;
 
+import net.heyzeer0.mgh.MagiHandlers;
 import net.heyzeer0.mgh.hacks.IEntity;
+import net.heyzeer0.mgh.hacks.ITileEntityOwnable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,7 +25,10 @@ public abstract class MixinExplosion {
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onConstruct(World w, Entity e, double x, double y, double z, float size, CallbackInfo ci) {
-        if ((exploder == null || !(exploder instanceof EntityPlayer)) && ((IEntity)e).hasOwner()) {
+        if (exploder == null && MagiHandlers.getStack().getFirst(ITileEntityOwnable.class).isPresent()) {
+            exploder = MagiHandlers.getStack().getFirst(ITileEntityOwnable.class).get().getFakePlayer();
+        }
+        if ((exploder == null || !(exploder instanceof EntityPlayer)) && e != null && ((IEntity)e).hasOwner()) {
             exploder = ((IEntity)e).getOwner();
         }
     }
