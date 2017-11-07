e7732dd22d89cb80afface5aaae98633b28870d6
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-11-07 12:52:18 -0200

    Botania spreader is now 100% solved

--- src/main/java/net/heyzeer0/mgh/mixins/botania/MixinTileSpreader.java
+++ src/main/java/net/heyzeer0/mgh/mixins/botania/MixinTileSpreader.java
@@ -2,29 +2,29 @@ package net.heyzeer0.mgh.mixins.botania;
 
 import net.heyzeer0.mgh.hacks.IEntityThrowable;
 import net.heyzeer0.mgh.hacks.ITileEntityOwnable;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.projectile.EntityThrowable;
-import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import vazkii.botania.common.block.tile.mana.TileSpreader;
+import vazkii.botania.common.entity.EntityManaBurst;
 
 /**
  * Created by Frani on 02/11/2017.
  */
 
 @Pseudo
-@Mixin(TileSpreader.class)
+@Mixin(value = TileSpreader.class, remap = false)
 public abstract class MixinTileSpreader implements ITileEntityOwnable {
 
-    @Redirect(method = "tryShootBurst", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z"))
-    private boolean onSpawnEntity(World world, Entity entity) {
-        if (entity instanceof EntityThrowable) {
+    @Redirect(method = "getBurst", at = @At(value = "INVOKE", target = "Lvazkii/botania/common/entity/EntityManaBurst;setSourceLens(Lnet/minecraft/item/ItemStack;)V"))
+    private void onSpawnEntity(EntityManaBurst entity, ItemStack lens) {
+        if (entity != null) {
             ((IEntityThrowable)entity).setThrower(getFakePlayer());
         }
-        return world.spawnEntityInWorld(entity);
+
+        entity.setSourceLens(lens);
     }
 
 }
