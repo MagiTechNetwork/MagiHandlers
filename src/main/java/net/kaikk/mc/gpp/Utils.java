ca526a4185614eee42620407ed658a1a4dbf778b
Eufranio <frani@magitechserver.com>
2017-11-22 21:18:29 -0200

    Workaround for GPP

---
+++ src/main/java/net/kaikk/mc/gpp/Utils.java
@@ -0,0 +1,14 @@
+package net.kaikk.mc.gpp;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Frani on 22/11/2017.
+ */
+public class Utils {
+
+    public static boolean isFakePlayer(Player player) {
+        return false;
+    }
+
+}
